7e6c19d37c93632dba3440e8324cbc95c3777788
ShotaNamerikawa <shota.namerikawa@gmail.com>
2024-07-18 17:19:02 +0900

    quit removing '&' from namelists' names

--- qe_utils/namelist.py
+++ qe_utils/namelist.py
@@ -1,6 +1,11 @@
 import re
 class NameList:
     """read namelist from file.
+    
+    Attributes
+    ----------
+    non_namelist_lines: List[str]
+        lines including strings not able to be parsed into namelists.
     """
     def __init__(self,file:str):
         self.file = file
@@ -8,7 +13,7 @@ class NameList:
         
     def read(self):
         self.namelist = dict() #namelist[card_name] = dict(key1->value1,...)
-        additional_lines = None #lines which are not cards.
+        non_namelist_lines = None #lines which are not cards.
         in_card = False # whether line is in card.
         
         with open(self.file) as fp:
@@ -18,7 +23,7 @@ class NameList:
             line = line.strip()
             if re.match("^ *&",line) is not None:
                 in_card = True
-                card_name = line.replace("&","")
+                card_name = line
                 self.namelist[card_name] = dict()
                 continue
                 
@@ -27,12 +32,12 @@ class NameList:
                 continue
     
             elif in_card == False:
-                additional_lines = lines[i:-1]
+                non_namelist_lines = lines[i:]
                 break
             
             key_value = [key_or_value.strip() for key_or_value in line.split("=")]
             self.namelist[card_name][key_value[0]] = key_value[1]
             
-        self.additional_lines = additional_lines
+        self.non_namelist_lines = non_namelist_lines # Non namelist lines.
             
                 
